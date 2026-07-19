f9f828db5417db39e5440ccec4e1fe0d6af64743
Priya Das <pd2820523@gmail.com>
2026-07-19 21:54:12 +0530

    Time: 3 ms (38.55%), Space: 19.3 MB (39.11%) - LeetHub

---
+++ 1081-smallest-subsequence-of-distinct-characters/1081-smallest-subsequence-of-distinct-characters.py
@@ -0,0 +1,31 @@
+class Solution:
+    def smallestSubsequence(self, s: str) -> str:
+        n = len(s)
+        taken = [False] * 26
+        lastoccurance = [-1] * 26
+        for i in range(n):
+            lastoccurance[ord(s[i]) - ord('a')] = i
+        monotonicstack = []
+        
+        for index in range(n):
+            char = s[index]
+            char_idx = ord(char) - ord('a')
+            if taken[char_idx]:
+                continue
+                
+            if len(monotonicstack) == 0:
+                monotonicstack.append(char)
+                taken[char_idx] = True
+            else:
+                curr = char
+                while monotonicstack and monotonicstack[-1] > curr:
+                    prev = monotonicstack[-1]
+                    nextoccur = lastoccurance[ord(prev) - ord('a')]
+                    if nextoccur > index:
+                        monotonicstack.pop()
+                        taken[ord(prev) - ord('a')] = False
+                    else:
+                        break
+                monotonicstack.append(char)
+                taken[char_idx] = True
+        return "".join(monotonicstack)
