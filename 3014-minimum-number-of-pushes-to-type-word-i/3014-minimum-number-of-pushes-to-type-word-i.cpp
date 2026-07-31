63ff34348c6aead131369457541018d9ad48384f
Priya Das <pd2820523@gmail.com>
2026-07-31 11:19:38 +0530

    Time: 0 ms (100%), Space: 8.4 MB (54.24%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -5,8 +5,15 @@ public:
         if(n<=8)return n;
         
         int res=0;
-        for(int i=0;i<n;i++){
-            res += (i/8)+1;
+        for(int i = 0; i < n; i++) {
+            if(i < 8)
+                res += 1;
+            else if(i < 16)
+                res += 2;
+            else if(i < 24)
+                res += 3;
+            else
+                res += 4;
         }
         return res;
     }
