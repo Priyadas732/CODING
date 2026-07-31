4b8243422679c99b26e205676278874cb2bbbc6e
Priya Das <pd2820523@gmail.com>
2026-07-31 12:21:54 +0530

    Time: 8 ms (61.48%), Space: 28.4 MB (31.38%) - LeetHub

--- 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.cpp
@@ -6,25 +6,12 @@ public:
         for(int i=0;i<n;i++){
             v[word[i]-'a']++;
         }
-        sort(v.begin(), v.end());
+        sort(v.rbegin(), v.rend());
         int res =0;
-        int no_ele=0;
-        for(int i=25;i>=0;i--){
-            if(no_ele<8 && v[i]>0){
-                res += v[i];
-                no_ele++;
-            }else if(no_ele<16 && v[i]>0){
-                res += 2*v[i];
-                no_ele++;
-            }else if(no_ele<24 && v[i]>0){
-                res += 3*v[i];
-                no_ele++;
-            }else{
-                if(v[i]>0){
-                    res += 4*v[i];
-                    no_ele++;
-                }
-            }
+        
+        for(int i=0;i<26;i++){
+            if(v[i]==0)break;
+            res += (i/8+1)*v[i];
         }
         return res;
     }
