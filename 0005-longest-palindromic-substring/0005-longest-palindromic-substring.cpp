c2386dc93cb7f8dd6d740f8a115561ae7d2bd273
Priya Das <pd2820523@gmail.com>
2026-08-04 18:11:33 +0530

    Time: 215 ms (18.47%), Space: 26.2 MB (23.42%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,31 +1,33 @@
 class Solution {
 public:
-    int n;
-    int dp[1001][1001];
-    bool isPalindrome(string &s,int i,int j){
-        
-        if(i>=j) return true;
 
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==s[j]){
-            return isPalindrome(s,i+1,j-1);
-        }
-        return dp[i][j] = 0;
-    }
-    
     string longestPalindrome(string s) {
-        n = s.size();
+        int n = s.size();
         
+        vector<vector<bool>> t(n, vector<bool>(n));
         int maxLen=0;
         int sp=0;
-        memset(dp,-1,sizeof(dp));
+        
         for(int i=0;i<n;i++){
-            for(int j=i;j<n;j++){
-                if(isPalindrome(s,i,j)==true){
+            t[i][i]=true;
+            maxLen= 1;
+        }
+        for(int L=2;L<=n;L++){
+            for(int i=0;i<n-L+1;i++){
+                int j = i+L-1;
+                if(s[i]==s[j] && L==2){
+                    t[i][j] = true;
+                    maxLen = 2;
+                    sp=i;
+                }
+                else if(s[i]==s[j] && t[i+1][j-1]==true){
+                    t[i][j] = true;
                     if(j-i+1 > maxLen){
                         maxLen = j-i+1;
-                        sp = i;
+                        sp=i;
                     }
+                }else{
+                    t[i][j] = false; // not a plaindrome
                 }
             }
         }
