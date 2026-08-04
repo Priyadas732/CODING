bfcaf64fc2dac4c8ee6d7d95050ddc419009b8f2
Priya Das <pd2820523@gmail.com>
2026-08-04 19:05:16 +0530

    Time: 0 ms (100%), Space: 7.7 MB (89.01%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    int dp[31];
-    int solve(int n){
-        if(n==0) dp[n]=0; 
-        if(n==1) dp[n]=1;
-        if(dp[n]!=-1) return dp[n];
-        return dp[n] = solve(n-1)+solve(n-2);
-    }
     int fib(int n) {
-        memset(dp,-1,sizeof(dp));
-        solve(n);
+        
+        int dp[n+1];
+        dp[0] = 0;
+        if(n==0) return dp[0];
+        dp[1] = 1;
+        if(n==1)return dp[1];        
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
         return dp[n];
     }
 };
