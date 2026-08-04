3e740578d1fdc12812d001e5fdfc1dbbd4061da4
Priya Das <pd2820523@gmail.com>
2026-08-04 17:28:33 +0530

    Time: 2256 ms (5%), Space: 13.8 MB (41.52%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int n;
-    
+    int dp[1001][1001];
     bool isPalindrome(string &s,int i,int j){
         
         if(i>=j) return true;
 
+        if(dp[i][j]!=-1) return dp[i][j];
         if(s[i]==s[j]){
             return isPalindrome(s,i+1,j-1);
         }
-        return false;
+        return dp[i][j] = 0;
     }
     
     string longestPalindrome(string s) {
@@ -17,6 +18,7 @@ public:
         
         int maxLen=0;
         int sp=0;
+        memset(dp,-1,sizeof(dp));
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
                 if(isPalindrome(s,i,j)==true){
