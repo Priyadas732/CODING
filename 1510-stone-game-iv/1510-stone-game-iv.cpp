130603410a8f11e98522ffbcf5b311b24863f5cb
Priya Das <pd2820523@gmail.com>
2026-08-10 14:24:50 +0530

    Time: 40 ms (64.21%), Space: 13.9 MB (56.63%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.cpp
+++ 1510-stone-game-iv/1510-stone-game-iv.cpp
@@ -1,35 +1,22 @@
 class Solution {
 public:
     int dp[100001];
+    bool solve(int n){
+        if(n==0)return false;
 
-    bool solve(int n) {
-        // No stones -> current player cannot move -> loses
-        if (n == 0)
-            return false;
+        if(dp[n]!=-1)return dp[n];
 
-        // Already calculated
-        if (dp[n] != -1)
-            return dp[n];
+        for(int i=1; i*i<=n;i++){
+            int remain = n-i*i;
 
-        // Try every perfect square
-        for (int i = 1; i * i <= n; i++) {
-
-            int remaining = n - i * i;
-
-            // If opponent gets a losing state,
-            // current player wins
-            if (solve(remaining) == false) {
+            if(solve(remain)==false){
                 return dp[n] = true;
             }
         }
-
-        // Every possible move gives opponent a winning state
         return dp[n] = false;
     }
-
     bool winnerSquareGame(int n) {
         memset(dp, -1, sizeof(dp));
-
         return solve(n);
     }
 };
