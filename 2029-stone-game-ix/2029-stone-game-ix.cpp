0b17659a2cec0ae85c45ef70647982d4cbcdde1d
Priya Das <pd2820523@gmail.com>
2026-08-16 15:48:27 +0530

    Time: 5 ms (48.73%), Space: 131.2 MB (69.29%) - LeetHub

---
+++ 2029-stone-game-ix/2029-stone-game-ix.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool stoneGameIX(vector<int>& stones) {
+        int cnt[3] = {0};
+
+        for (int x : stones) {
+            cnt[x % 3]++;
+        }
+
+        if (cnt[0] % 2 == 0)
+            return min(cnt[1], cnt[2]) > 0;
+        else
+            return abs(cnt[1] - cnt[2]) > 2;
+
+        return false;    
+    }
+};
