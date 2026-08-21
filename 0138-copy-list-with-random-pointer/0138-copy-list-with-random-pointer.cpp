71923b6e281ba076b5daf92819180d29ae8af930
Priya Das <pd2820523@gmail.com>
2026-08-21 19:47:44 +0530

    Time: 8 ms (27.6%), Space: 14.9 MB (86.86%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -19,43 +19,44 @@ public:
     Node* copyRandomList(Node* head) {
 
         if(head==NULL) return NULL;
-        unordered_map<Node*, Node*>mp;
-
+        
+        //1) Insert the new nodes between the original linkedlist
         Node* curr = head;
-        Node* prev = NULL;
-        Node* newHead = NULL;
+        
+        while(curr){
+            Node* currNext = curr->next;
+            curr->next = new Node(curr->val); // A->B
 
-        while(curr){ 
-            Node* temp = new Node(curr->val);
+            curr->next->next = currNext;      // A->X->B
 
-            mp[curr] = temp; // storing
-
-            if(newHead==NULL){
-                newHead = temp;
-                prev = newHead;
-            }else{
-                prev->next = temp;
-                prev = temp;
-            }
-
-            curr = curr->next;
+            curr = currNext;
         }
 
-        // Fill random pointer
+        //2) Deep copy of random poniters
         curr = head;
-        Node* newCurr = newHead;
-
-        while(curr){
+        while(curr && curr->next){
             if(curr->random==NULL){
-                newCurr->random = NULL;
+                curr->next->random = NULL;
             }else{
-                newCurr->random = mp[curr->random];
+                curr->next->random = curr->random->next;
             }
+
+            curr = curr->next->next;
+        }
+
+        //3) Separate the linked lists
+        Node* newHead = head->next;
+        Node* newCurr = newHead;
+        curr = head;
+
+        while(curr && newCurr){
+            curr->next = curr->next == NULL? NULL: curr->next->next;
+
+            newCurr->next = newCurr->next == NULL? NULL: newCurr->next->next;
 
             curr = curr->next;
             newCurr = newCurr->next;
         }
-
         return newHead;
     }
 };
