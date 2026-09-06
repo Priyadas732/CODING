845519175cd7798a0fe667991616808658507123
Priya Das <pd2820523@gmail.com>
2026-09-06 09:59:09 +0530

    Time: 0 ms (100%), Space: 18.2 MB (92.81%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -10,40 +10,34 @@
  */
 class Solution {
 public:
-    ListNode* mergeTwosortedLists(ListNode* l1, ListNode* l2){
-        if(!l1) return l2;
-        if(!l2) return l1;
-
-        if(l1->val < l2->val){
-            l1->next = mergeTwosortedLists(l1->next, l2);
-            return l1;
-        }else{
-            l2->next = mergeTwosortedLists(l1, l2->next);
-            return l2;
+    struct Compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val;  // Min-heap
         }
-
-        return NULL;
-    }
-    ListNode* partitionAndMerge(int start, int end, vector<ListNode*>& lists){
-        if(start > end){
-            return NULL;
-        }
-
-        if(start == end){
-            return lists[start];
-        }
-
-        int mid = start + (end - start)/2;
-        ListNode* L1 = partitionAndMerge(start, mid, lists);
-        ListNode* L2 = partitionAndMerge(mid+1, end, lists);
-
-        return mergeTwosortedLists(L1, L2);
-    }
+    };
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        int k = lists.size();
+        priority_queue<ListNode*, vector<ListNode*>, Compare>pq;
 
-        if(k == 0) return NULL;
+        for(auto head: lists){
+            if(head != nullptr)
+                pq.push(head);
+        }
 
-        return partitionAndMerge(0, k-1, lists);
+        ListNode* head = NULL;
+        ListNode* tail = head;
+        while(!pq.empty()){
+            ListNode* curr = pq.top();
+            pq.pop();
+            if(curr->next !=NULL)pq.push(curr->next);
+            if(head==NULL){
+                head = curr;
+                tail = head;
+            }else{
+                tail->next =  curr;
+                tail = tail->next;
+            }
+        }
+
+        return head;
     }
 };
