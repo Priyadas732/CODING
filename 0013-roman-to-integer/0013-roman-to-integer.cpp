bd19bc0865cbae84d94acf6d642e58c4c86656aa
Priya Das <pd2820523@gmail.com>
2026-09-12 18:28:57 +0530

    Time: 4 ms (66.34%), Space: 12.3 MB (67.02%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.cpp
+++ 0013-roman-to-integer/0013-roman-to-integer.cpp
@@ -2,43 +2,26 @@ class Solution {
 public:
     int romanToInt(string s) {
         int n = s.size();
-        int num = 0;
-        for(int i = 0; i < n; i++){
-            if(s[i] == 'I' && i + 1 < n && s[i+1] == 'V'){
-                num += 4;
-                i++;
+        unordered_map<char,int>mp;
+
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+
+        int ans = 0;
+        for(int i=0;i<n;i++){
+            if(i+1<n && mp[s[i]] < mp[s[i+1]]){
+                ans -= mp[s[i]];
             }
-            else if(s[i] == 'I' && i + 1 < n && s[i+1] == 'X'){
-                num += 9;
-                i++;
-            }
-            else if(s[i] == 'X' && i + 1 < n && s[i+1] == 'L'){
-                num += 40;
-                i++;
-            }
-            else if(s[i] == 'X' && i + 1 < n && s[i+1] == 'C'){
-                num += 90;
-                i++;
-            }
-            else if(s[i] == 'C' && i + 1 < n && s[i+1] == 'D'){
-                num += 400;
-                i++;
-            }
-            else if(s[i] == 'C' && i + 1 < n && s[i+1] == 'M'){
-                num += 900;
-                i++;
-            }
-            else {
-                if(s[i] == 'I') num += 1;
-                if(s[i] == 'V') num += 5;
-                if(s[i] == 'X') num += 10;
-                if(s[i] == 'L') num += 50;
-                if(s[i] == 'C') num += 100;
-                if(s[i] == 'D') num += 500;
-                if(s[i] == 'M') num += 1000;
+            else{
+                ans += mp[s[i]];
             }
         }
 
-        return num;
+        return ans;
     }
 };
