105c49822e89971d9b7645b305bdc66f5f0e94ba
Priya Das <pd2820523@gmail.com>
2026-09-12 22:30:23 +0530

    Time: 10 ms (12.17%), Space: 11 MB (71.58%) - LeetHub

---
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -0,0 +1,108 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        unordered_map<char, int>mp;
+        mp['I'] = 1;
+        mp['V'] = 5;
+        mp['X'] = 10;
+        mp['L'] = 50;
+        mp['C'] = 100;
+        mp['D'] = 500;
+        mp['M'] = 1000;
+
+        string s = "";
+        while(num>0){
+            // 1000-900
+            if(num >= 1000){
+                int k = num/1000;
+                while(k>0){
+                    s += 'M';
+                    k--;
+                }
+
+                num %= 1000;
+            }
+            else if(num >= 900){
+                s += "CM";
+                num %= 900;
+            }
+            // 500-400
+            else if(num >= 500){
+                int k = num/500;
+                while(k>0){
+                    s += 'D';
+                    k--;
+                }
+
+                num %= 500;
+            }
+            else if(num >= 400){
+                s += "CD";
+                num %= 400;
+            }
+            //100-90
+            else if(num >= 100){
+                int k = num/100;
+                while(k>0){
+                    s += 'C';
+                    k--;
+                }
+
+                num %= 100;
+            }
+            else if(num >= 90){
+                s += "XC";
+                num %= 90;
+            }
+            // 50-40
+            else if(num >= 50){
+                int k = num/50;
+                while(k>0){
+                    s += 'L';
+                    k--;
+                }
+
+                num %= 50;
+            }
+            else if(num >= 40){
+                s += "XL";
+                num %= 40;
+            }
+            // 10-9
+            else if(num >= 10){
+                int k = num/10;
+                while(k>0){
+                    s += 'X';
+                    k--;
+                }
+
+                num %= 10;
+            }
+            else if(num >= 9){
+                s += "IX";
+                num %= 9;
+            }
+            // 5-4
+            else if(num >= 5){
+                int k = num/5;
+                while(k>0){
+                    s += 'V';
+                    k--;
+                }
+
+                num %= 5;
+            }
+            else if(num >= 4){
+                s += "IV";
+                num %= 4;
+            }
+
+            else{
+                s += 'I';
+                num --;
+            }
+        }
+
+        return s;
+    }
+};
