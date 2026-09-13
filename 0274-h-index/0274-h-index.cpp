551ee8f11a8d4a303551b9aaa8aba440a51e7aa0
Priya Das <pd2820523@gmail.com>
2026-09-13 23:46:42 +0530

    Time: 1 ms (18.53%), Space: 13.9 MB (5.31%) - LeetHub

--- 0274-h-index/0274-h-index.cpp
+++ 0274-h-index/0274-h-index.cpp
@@ -3,11 +3,14 @@ public:
     int hIndex(vector<int>& citations) {
         sort(citations.rbegin(), citations.rend());
 
-        for(int i=0;i<citations.size();i++){
-            if(citations[i] < i+1)return i;
+        for (int i = 0; i < citations.size(); i++) {
+            // The (i+1)-th paper does not have at least (i+1) citations
+            if (citations[i] < i + 1) {
+                return i; // Exactly i papers qualified
+            }
         }
 
+        // All papers qualified
         return citations.size();
-
     }
 };
