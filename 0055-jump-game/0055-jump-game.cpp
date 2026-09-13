14070583c37946adf39fc7dd5ec4a38ab37684d4
Priya Das <pd2820523@gmail.com>
2026-09-13 21:31:55 +0530

    Time: 725 ms (5.01%), Space: 55.9 MB (6.07%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int>v(n, false);
-        v[0] = true;
-        for(int i=1;i<n;i++){ // O(n^2)
-            int j = i-1;
-            while(j>=0){
-                if(v[j]==true && j+nums[j] >= i){
-                    v[i]=true;
-                    break;
-                }
-                j--;
-            }
+    int n;
+    bool go(int idx, vector<int>& nums, vector<int>&dp){
+        if(idx==n-1)return true;
+        if(nums[idx]==0)return false;
+
+        if(dp[idx] !=-1)return dp[idx];
+        for(int i=1;i<=nums[idx];i++){
+            if(go(idx+i, nums,dp)) return dp[idx] = true;
         }
-        return v[n-1];
+        return dp[idx] = false;
+    }
+    bool canJump(vector<int>& nums) {
+        n = nums.size();
+        vector<int>dp(n,-1);
+        return go(0, nums,dp);
     }
 };
